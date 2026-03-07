c5c1d2f498b507fe6e21c60195b340a18d97dd4d
amarmeshram567 <meshramamar567@gmail.com>
2026-03-07 09:19:35 +0530

    url added

--- frontend/src/lib/app.js
+++ frontend/src/lib/app.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_BACKEND_URL || "https://unifiedopshakathon-server.onrender.com";
+const API_BASE = import.meta.env.VITE_BACKEND_URL || "https://unifiedopshakathon-project.onrender.com";
 
 
 
